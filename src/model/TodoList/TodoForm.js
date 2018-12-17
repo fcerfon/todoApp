87d8a343db9355ca72bcfb0157ee7a643b98181f
fcerfon <florent.cerfon@gmail.com>
2018-12-17 10:06:09 +0100

    new todo fixed and reset list button done

--- src/model/TodoList/TodoForm.js
+++ src/model/TodoList/TodoForm.js
@@ -4,13 +4,19 @@ export default class TodoForm extends Component {
 
     constructor() {
         super();
-        this.onFormSubmitted = this.onFormSubmitted.bind(this)
+        this.onFormSubmitted = this.onFormSubmitted.bind(this);
+        this.resetTodos = this.resetTodos.bind(this);
     }
 
     onFormSubmitted(event) {
         event.preventDefault();
         var form = event.target;
         this.props.onDone(form.elements['todo_text'].value);
+    }
+
+    resetTodos(event) {
+        event.preventDefault();
+        this.props.reset();
     }
 
     render() {
@@ -21,6 +27,9 @@ export default class TodoForm extends Component {
                 </section>
                 <section>
                     <input type='submit' />
+                </section>
+                <section>
+                    <button type='button' onClick={this.resetTodos}>Vider la liste</button>
                 </section>
             </form>
         );
